910c5e3b42811b133d19bf37a6b460550c85e808
Herman Kudria <h.kudrya@hotmail.com>
2022-10-12 13:19:02 +0200

    feat: solve transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,5 @@
 const { NotImplementedError } = require('../extensions/index.js');
+const {countCats} = require("./count-cats");
 
 /**
  * Create transformed array based on the control sequences that original
@@ -13,9 +14,66 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)){
+    throw Error('\'arr\' parameter must be an instance of the Array!');
+  }
+  let copyArr = [...arr]
+  let count = 0
+  let lastModIndex = -1000
+  arr.forEach((el,index) => {
+    switch (el){
+      case '--double-next':
+        if(copyArr[index+1]){
+          copyArr[index+count] = copyArr[index+1+count]
+          lastModIndex = index
+        } else {
+          copyArr.splice(index,1)
+        }
+        break
+      case '--discard-prev':
+        if(copyArr[index-1] ){
+          if(lastModIndex === index-1){
+            copyArr.splice(index+count , 1)
+            count--
+          } else {
+            copyArr.splice(index + count - 1,2)
+            count -=2
+            lastModIndex = index
+          }
+        } else {
+          copyArr.splice(index , 1)
+          count--
+        }
+        break
+      case '--discard-next':
+        if(copyArr[index+1]) {
+          copyArr.splice(index + count, 2)
+          count -= 2
+          lastModIndex = index+1
+        } else {
+          copyArr.splice(index,1)
+        }
+        break
+      case '--double-prev':
+        if(copyArr[index-1+count]) {
+          if(lastModIndex>index-1+count){
+            copyArr.splice(index+count , 1)
+            count--
+          } else {
+            copyArr[index+count] = copyArr[index - 1+count]
+            lastModIndex = index
+          }
+
+        } else {
+          copyArr.splice(index+count , 1)
+          count--
+        }
+        break
+    }
+  })
+
+  return copyArr
 }
 
 module.exports = {
