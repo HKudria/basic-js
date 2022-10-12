8e17e813a1bb536a30659f1e79f07d183aef10c0
Herman Kudria <h.kudrya@hotmail.com>
2022-10-12 14:40:55 +0200

    feat: solve vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,61 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  letter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+  constructor(mode) {
+    this.mode = mode ?? true
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(message, key) {
+    if (message === undefined || key === undefined) {
+      throw Error('Incorrect arguments!')
+    }
+    let result = ''
+    let offset = 0
+    for (let i = 0; i < message.length; i++) {
+      let s = message[((i >= message.length) ? i % message.length : i)].toUpperCase()
+      let mi = this.letter.indexOf(s);
+      if (mi === -1) {
+        result += s
+        offset++
+        continue
+      }
+      let ki_s = key[((i - offset >= key.length) ? (i - offset) % key.length : i - offset)]
+      let ki = this.letter.indexOf(ki_s.toUpperCase())
+      let c = this.letter[(((this.letter.length + (mi + ki)) % this.letter.length))]
+      result += c;
+    }
+    if (this.mode) {
+      return result.toUpperCase();
+    } else {
+      return result.toUpperCase().split("").reverse().join("")
+    }
+  }
+  decrypt(encryptedMessage, key) {
+    if (encryptedMessage === undefined || key === undefined) {
+      throw Error('Incorrect arguments!')
+    }
+    let result = ''
+    let offset = 0
+    for (let i = 0; i < encryptedMessage.length; i++) {
+      let s = encryptedMessage[((i >= encryptedMessage.length) ? i % encryptedMessage.length : i)].toUpperCase()
+      let mi = this.letter.indexOf(s)
+      if (mi === -1) {
+        result += s
+        offset++
+        continue
+      }
+      let ki_s = key[((i - offset >= key.length) ? (i - offset) % key.length : i - offset)]
+      let ki = -this.letter.indexOf(ki_s.toUpperCase())
+      let c = this.letter[(((this.letter.length + (mi + ki)) % this.letter.length))]
+      result += c
+    }
+    if (this.mode) {
+      return result.toUpperCase();
+    } else {
+      return result.toUpperCase().split("").reverse().join("")
+    }
   }
 }
 
